1fe417f62eea92e3ea19e96298ec567d4044d2cd
ابوالحسن علي عبدالرزاق <abu.ali2007@ircoedu.uobaghdad.edu.iq>
2026-06-20 14:55:41 +0300

    Add tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#dc2626',
+        secondary: '#111827',
+      },
+    },
+  },
+  plugins: [],
+}
